9c2e4fa2bcf859d72f29e835bf7a6ec2284990f7
Yohanna8 <yohanna.cavalcanti@dock.tech>
2024-11-13 21:30:06 -0300

    ADD MORE FUNC

--- rede_multicamanda_sigmoid.py
+++ rede_multicamanda_sigmoid.py
@@ -6,6 +6,10 @@ def sigmoid(soma):
     return 1 / (1 + np.exp(-soma))
 
 
+def simoid_derivada(sig):
+    return sig * (1 - sig)
+
+
 entradas = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 saidas = np.array([[0], [1], [1], [0]])
 # sinapse entrada -> camada oculta
@@ -16,7 +20,7 @@ pesos_camada_oculta_para_saida = np.array([[-0.017], [-0.893], [0.148]])
 epocas = 100
 
 camada_entrada = entradas.copy()
-print(camada_entrada)
+
 for j in range(epocas):
     soma_sinapse_x1_x2 = np.dot(camada_entrada, pesos_x1_x2)
     camada_oculta = sigmoid(soma_sinapse_x1_x2)
@@ -24,4 +28,14 @@ for j in range(epocas):
     soma_sinapse_oculta = np.dot(camada_oculta, pesos_camada_oculta_para_saida)
     camada_saida = sigmoid(soma_sinapse_oculta)
 
-print(camada_saida)
+    erro_camada_saida = saidas - camada_saida
+    media_absoluta_saida = np.mean(np.abs(erro_camada_saida))
+
+    derivada_saida = simoid_derivada(camada_saida)
+    delta_saida = erro_camada_saida * derivada_saida
+
+    pesos_x1_x2_transpostas = pesos_camada_oculta_para_saida.T
+    delta_saida_x_peso = delta_saida.dot(pesos_x1_x2_transpostas)
+    delta_camada_oculta = delta_saida_x_peso * simoid_derivada(camada_oculta)
+
+print(np.round(delta_camada_oculta, decimals=3))
